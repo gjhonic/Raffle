37aa7bdd1cea0c9697af48d31dedd7b2efccb710
denis-dubatolov <denis-dubatolov@mail.ru>
2021-04-11 15:59:22 +0300

    Made a CRUD for the Tag table

---
+++ app/Models/Tag.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "tag".
+ *
+ * @property int $id
+ * @property string $title
+ */
+class Tag extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'tag';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'required'],
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+        ];
+    }
+}
