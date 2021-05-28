5b1a3956638d3e0b5b97fb66e5e36dca5c13823a
DiGjhonic <45502448+gjhonic@users.noreply.github.com>
2021-05-28 22:08:29 +0800

    Небольшие правки по задаче

--- app/views/raffle/view.php
+++ app/views/raffle/view.php
@@ -1,9 +1,11 @@
 <?php
-/* @var $user object */
+use yii\helpers\Url;
+/* @var $model object */
+/* @var $author object */
 $this->title = $model->title;
 ?>
 <header id="header">
-    <a href="index.html" class="logo"><strong>Show/<?=$model->code?></strong></a>
+    <a href="#" class="logo"><span onclick="window.location.replace('<?=URL::to('/raffle/index')?>');">Конкурсы / </span><?=$model->code?></a>
     <p class="icons"><?=date('m.d.Y', $model->created_at)?></p>
 </header>
 <section>
@@ -12,6 +14,7 @@ $this->title = $model->title;
     </header>
     <span class="image main"><img src="/app/media/src/raffle/pic11.jpg" alt=""></span>
     <time class="published" datetime="2015-11-01">01.01.21 - 05.01.21</time></br></br>
-    <a href="#" class="button"><span class="name">Jane Doe</span></a>
-    </br></br><p><?=$model->description?></p>
+    <a href="<?=URL::to('/profile').'/'.$author->code?>" class="button">Автор: <?=$author->username?></span></a>
+    </br></br>
+    <p><?=$model->description?></p>
 </section>
