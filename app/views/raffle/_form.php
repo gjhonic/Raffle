e0737938a95c76d6ce6df1438f289c201640f7d6
DiGjhonic <45502448+gjhonic@users.noreply.github.com>
2021-07-01 00:00:41 +0800

    Добавил кнопку генерации кода

--- app/views/raffle/_form.php
+++ app/views/raffle/_form.php
@@ -23,12 +23,25 @@ use kartik\datetime\DateTimePicker;
     </p>
 
     <p>
-        <?= $form->field($model, 'code')->hint('Отображается в адресной строке, оставьте пустым и произойдет автоматическая генерация')->textInput(['maxlength' => true]) ?>
+    <h5>Код</h5>
+    <div class="row">
+        <div class="col-6 col-12-small">
+            <p>
+                <input type="text" id="input-code" placeholder="Код" name="RaffleForm[code]" maxlength='25'>
+            </p>
+        </div>
+        <div class="col-6 col-12-small">
+            <p>
+                <span class="button primary fit" onclick="codeGenerate()">Сгенерировать код</span>
+            </p>
+        </div>
+        <span>Отображается в адресной строке, оставьте пустым и произойдет автоматическая генерация</span>
+    </div>
     </p>
 
     <div class="form-group">
         <label for="date_begin" class="control-label">Дата начала конкурса</label>
-        <input type="date" class="form-control" id="date_begin" placeholder="" name="RaffleForm[date_begin]" value="<?=date('Y-m-d')?>" min="2021-01-01" max="2050-12-31">
+        <input type="date" 0 class="form-control" id="date_begin" placeholder="" name="RaffleForm[date_begin]" value="<?=date('Y-m-d')?>" min="2021-01-01" max="2050-12-31">
     </div>
 
     <p>
@@ -37,3 +50,15 @@ use kartik\datetime\DateTimePicker;
     </p>
 
 <?php ActiveForm::end(); ?>
+
+<script>
+    function codeGenerate(){
+        let result           = '';
+        let characters       = 'abcdefghijklmnopqrstuvwxyz0123456789';
+        let charactersLength = characters.length;
+        for ( let i = 0; i < 25; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        document.getElementById('input-code').value = result;
+    }
+</script>
