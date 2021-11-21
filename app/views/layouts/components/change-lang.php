951d57a124a4ef1c45db214d1cf8985be1337679
DiGjhonic <45502448+gjhonic@users.noreply.github.com>
2021-11-21 14:06:38 +0800

    refactor: revision

--- app/views/layouts/components/change-lang.php
+++ app/views/layouts/components/change-lang.php
@@ -5,11 +5,11 @@ use yii\helpers\Url;
 $lang = Yii::$app->language;
 $currentUrl = mb_substr(Url::to(), 4);
 
-$langEnActive = ($lang == "en") ? 'button primary' : "button";
-$langRuActive = ($lang == "ru") ? 'button primary' : "button";
+$langEnActive = ($lang === "en") ? 'button primary' : "button";
+$langRuActive = ($lang === "ru") ? 'button primary' : "button";
 ?>
 
 <div class="change-lang">
-    <a href="/en/<?=$currentUrl?>" class="<?=$langEnActive?>">en</a>
-    <a href="/ru/<?=$currentUrl?>" class="<?=$langRuActive?>">ru</a>
+    <a href="/en/<?=$currentUrl?>" class="<?=$langEnActive?>" title="<?=Yii::t('app', 'English')?>">en</a>
+    <a href="/ru/<?=$currentUrl?>" class="<?=$langRuActive?>" title="<?=Yii::t('app', 'Russian')?>">ru</a>
 </div>
