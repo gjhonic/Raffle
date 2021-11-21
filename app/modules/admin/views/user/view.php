06ce6db5f8a0b0fd9b0f9cfc4ccc80b8acfcec79
DiGjhonic <45502448+gjhonic@users.noreply.github.com>
2021-11-21 16:09:49 +0800

    refactor: revision in admin module

--- app/modules/admin/views/user/view.php
+++ app/modules/admin/views/user/view.php
@@ -5,6 +5,7 @@
 use yii\helpers\Html;
 use yii\widgets\DetailView;
 use yii\helpers\Url;
+use app\models\db\User;
 
 
 $this->title = $model->surname." ".$model->name." (".$model->username.")";
@@ -15,6 +16,46 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="user-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+
+    <?php if($model->status_id == User::STATUS_TAG_TO_BAN_ID){ ?>
+
+        <?=Html::a('Отметить бан', Url::to('/admin/user-mod/untag')."?id=".$model->id, [
+                'class' => 'btn btn-outline-danger btn-block',
+                'data' => [
+                    'confirm' => 'Вы действительнос хотите отметить бан пользователя '.$model->username.'?',
+                    'method' => 'post',
+                ]])?>
+
+        <?=Html::a('Забанить', Url::to('/admin/user-mod/ban')."?id=".$model->id, [
+                'class' => 'btn btn-outline-dark btn-block',
+                'data' => [
+                    'confirm' => 'Вы действительнос хотите забанить пользователя '.$model->username.'?',
+                    'method' => 'post',
+                ]])?>
+    <?php } elseif($model->status_id == User::STATUS_ACTIVE_ID){ ?>
+        <?=Html::a('Пометить бан', Url::to('/admin/user-mod/tag')."?id=".$model->id, [
+                'class' => 'btn btn-outline-danger btn-block',
+                'data' => [
+                    'confirm' => 'Вы действительнос хотите пометить бан на пользователя '.$model->username.' ?',
+                    'method' => 'post',
+                ]]) ?>
+
+        <?=Html::a('Забанить', Url::to('/admin/user-mod/ban')."?id=".$model->id, [
+                'class' => 'btn btn-outline-dark btn-block',
+                'data' => [
+                    'confirm' => 'Вы действительнос хотите забанить пользователя '.$model->username.'?',
+                    'method' => 'post',
+                ]])?>
+    <?php } else { ?>
+        <?=Html::a('Разбанить', Url::to('/admin/user-mod/unban')."?id=".$model->id, ['class' => 'btn btn-outline-warning btn-block','data' => [
+                'confirm' => 'Вы действительнос хотите разбанить пользователя '.$model->username.'?',
+                'method' => 'post',
+            ]])?>
+    <?php } ?>
+
+    </p>
 
     <?= DetailView::widget([
         'model' => $model,
